9986793348563cfbf9d0f3662a1114baa72c6f4a
idktigerz <idktigerzz@gmail.com>
2022-04-21 08:59:13 +0100

    commit

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -6,8 +6,8 @@ const room = sessionStorage.getItem("roomId");
 
 async function getCardByID(cardId){
     let cardInfo = await requestCardInfo(cardId);
-    document.getElementById("cardName").innerHTML = cardInfo.cards_crd_name;
-    document.getElementById("cardType").innerHTML = cardInfo.cards_types_tp_name;
-    document.getElementById("cardCost").innerHTML = cardInfo.card_crd_cost;
-    document.getElementById("cardAttack").innerHTML = cardInfo.cards_crd_atk;
+    document.getElementById("cardName").innerHTML = cardInfo.crd_name;
+    document.getElementById("cardType").innerHTML = cardInfo.tp_name;
+    document.getElementById("cardCost").innerHTML = cardInfo.crd_cost;
+    document.getElementById("cardAttack").innerHTML = cardInfo.crd_atk;
 }
